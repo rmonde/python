84173487b9f37a3b172e3f89e1ec592182f18831
rmonde <monde_rahul@yahoo.co.in>
2018-12-15 16:00:18 +0530

    My first assignment

---
+++ main.py
@@ -0,0 +1,14 @@
+'''This is my first assginment'''
+# Declaring variables
+song_name = 'Swaye shree Ram prabhu aikati'
+artist = 'Sudhir Phadake'
+gener = 'bhakti-geet'
+yr_published = "1965"
+composed_by = 'G. D. Madgulkar'
+no_of_copies = 100000
+
+print('My favorite song is: ',song_name)
+print('Name of the singer is:',artist)
+print('This song was composed by:',composed_by)
+print('The gener of the song is', gener , 'and it was published in ',yr_published)
+print('This song became popular amongst every one so much that there were ',no_of_copies, 'copies sold')
